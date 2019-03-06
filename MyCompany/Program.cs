6f9bb45f5d695c3d0b42eea0b4f071e37bf241b8
Marshall Offutt <marshalloffutt@gmail.com>
2019-03-06 11:36:39 -0600

    finishes the exercise

--- MyCompany/Program.cs
+++ MyCompany/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyCompany
 {
@@ -6,7 +7,29 @@ namespace MyCompany
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Instantiate a new company
+            var theBeatlesCo = new Company("The Beatles Company", new DateTime());
+
+            //Instantiate 4 new employees
+            var john = new Employee("John", "Sr. Software Developer", new DateTime());
+            var paul = new Employee("Paul", "Data Analyst", new DateTime());
+            var george = new Employee("George", "Project Manager", new DateTime());
+            var ringo = new Employee("Ringo", "Junior Developer", new DateTime());
+
+            //Add new employees to list
+            theBeatlesCo.addNewEmployee(john);
+            theBeatlesCo.addNewEmployee(paul);
+            theBeatlesCo.addNewEmployee(george);
+            theBeatlesCo.addNewEmployee(ringo);
+
+            //Remove john from list because of yoko :(
+            theBeatlesCo.removeEmployee(john);
+
+            //List the employees
+            theBeatlesCo.ListEmployees();
+
+            Console.ReadKey();
         }
     }
+
 }
